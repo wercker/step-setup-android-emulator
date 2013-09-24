53c8b8b41c4c2c169f8f1da6637c8bdb944df2d5
Jacco Flenter <github@putpixel.nl>
2013-09-24 14:52:31 +0200

    use WERCKER_STEP_ROOT

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 
-
 echo no | android create avd --force -n $WERCKER_SETUP_ANDROID_EMULATOR_NAME -t $WERCKER_SETUP_ANDROID_EMULATOR_TARGET --abi $WERCKER_SETUP_ANDROID_EMULATOR_ABI
 emulator -avd $WERCKER_SETUP_ANDROID_EMULATOR_NAME -no-skin -no-audio -no-window  &
-./wait_for_emulator.sh
+$WERCKER_STEP_ROOT/wait_for_emulator.sh
