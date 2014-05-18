b8ae1a468ab20ffb81a01bc6368cd2149448b812
Vikrant Varma <vikrant.varma94@gmail.com>
2014-05-19 05:02:17 +0530

    Enable hardware keyboard
    This reduces flakiness of tests.

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 
 echo no | android create avd --force -n $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -t $WERCKER_SETUP_ANDROID_EMULATOR_TARGET --abi $WERCKER_SETUP_ANDROID_EMULATOR_ABI
+echo "hw.keyboard=yes" >> ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
+echo "Enabled hardware keyboard"
+cat ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
 emulator -avd $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -no-audio -no-window &
 $WERCKER_STEP_ROOT/wait_for_emulator.sh
